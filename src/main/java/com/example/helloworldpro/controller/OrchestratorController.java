2745d6f15ec2f4b0bc114757849043df1c071ea2
IrinaPostnikova <iri.postnikova@gmail.com>
2022-09-09 00:18:19 +0300

    kafka for parser module

--- src/main/java/com/example/helloworldpro/controller/OrchestratorController.java
+++ src/main/java/com/example/helloworldpro/controller/OrchestratorController.java
@@ -1,5 +1,6 @@
 package com.example.helloworldpro.controller;
 
+import com.example.helloworldpro.kafka.MessageListener;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.CloseableHttpClient;
@@ -10,16 +11,17 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.URL;
 
 @RestController
 @CrossOrigin("*")
 @RequestMapping("/api/orchestra")
 public class OrchestratorController {
+private MessageListener messageListener;
+
+    public OrchestratorController(MessageListener messageListener) {
+        this.messageListener = messageListener;
+    }
 
     @GetMapping
     public String sendResponse() throws IOException {
@@ -71,6 +73,8 @@ public class OrchestratorController {
             // Закрыть браузер
             httpClient.close();
         }
+
         return resp;
     }
+
 }
