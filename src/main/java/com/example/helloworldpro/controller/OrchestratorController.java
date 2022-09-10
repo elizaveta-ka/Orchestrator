3df0b9041a7ec0dbd4b5d481e207d9371f5dd7df
elizavetakabak <kabakelizaveta@gmail.com>
2022-09-10 12:12:04 +0300

    kafka with Parser Controller + Orchestrator(MessageListener)

--- src/main/java/com/example/helloworldpro/controller/OrchestratorController.java
+++ src/main/java/com/example/helloworldpro/controller/OrchestratorController.java
@@ -1,5 +1,6 @@
 package com.example.helloworldpro.controller;
 
+import com.example.helloworldpro.model.Product;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.CloseableHttpClient;
@@ -17,39 +18,45 @@ import java.io.IOException;
 @RequestMapping("/api/orchestra")
 public class OrchestratorController {
 
-    @GetMapping
+//    @GetMapping
+//    @KafkaListener(topics = "${kafka.topic.name}", containerFactory = "kafkaListenerContainerFactory")
+//    public String sendResponse() throws IOException {
+//
+//        // Создать Httpclient, который эквивалентен открытию браузера
+//        CloseableHttpClient httpClient = HttpClients.createDefault();
+//        // Создать запрос HttpGet, эквивалентный вводу адреса в браузере
+//        HttpGet httpGet = new HttpGet("http://localhost:8082/api/hello");
+//        String resp = null;
+//        CloseableHttpResponse response = null;
+//
+//        try {
+//            // Выполнить запрос, который эквивалентен нажатию Enter после выбивания адреса, чтобы получить ответ
+//            response = httpClient.execute(httpGet);
+//
+//            // Определяем, равен ли статус возврата 200
+////            if (response.getStatusLine().getStatusCode() == 200) {
+//                // Анализируем ответ и получаем данные
+//                String content = EntityUtils.toString(response.getEntity(), "UTF-8");
+//                    resp = content;
+////            }
+//
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        } finally {
+//            if (response != null) {
+//                // Закрыть ресурсы
+//                response.close();
+//            }
+//            // Закрыть браузер
+//            httpClient.close();
+//        }
+//
+//        return resp;
+//    }
+
     @KafkaListener(topics = "${kafka.topic.name}", containerFactory = "kafkaListenerContainerFactory")
-    public String sendResponse() throws IOException {
-        // Создать Httpclient, который эквивалентен открытию браузера
-        CloseableHttpClient httpClient = HttpClients.createDefault();
-        // Создать запрос HttpGet, эквивалентный вводу адреса в браузере
-        HttpGet httpGet = new HttpGet("http://localhost:8082/api/hello");
-        String resp = null;
-        CloseableHttpResponse response = null;
-
-        try {
-            // Выполнить запрос, который эквивалентен нажатию Enter после выбивания адреса, чтобы получить ответ
-            response = httpClient.execute(httpGet);
-
-            // Определяем, равен ли статус возврата 200
-            if (response.getStatusLine().getStatusCode() == 200) {
-                // Анализируем ответ и получаем данные
-                String content = EntityUtils.toString(response.getEntity(), "UTF-8");
-                System.out.println(content);
-                    resp = content;
-            }
-
-        } catch (IOException e) {
-            e.printStackTrace();
-        } finally {
-            if (response != null) {
-                // Закрыть ресурсы
-                response.close();
-            }
-            // Закрыть браузер
-            httpClient.close();
-        }
-
-        return resp;
+    public void listener(Product product) {
+        System.out.println("Recieved message: " + product);
+//        productRepository.save(product);
     }
 }
